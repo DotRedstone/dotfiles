20b3bcaaacc91dea0f65563afdc806ef334995d8
dotredstone <2724236945@qq.com>
2026-05-06 03:42:33 +0800

    feat(nixvim): 完成全系统快捷键菜单汉化，配置 Ctrl+/ 终端快捷键

--- home/dot/nixvim/plugins/git.nix
+++ home/dot/nixvim/plugins/git.nix
@@ -24,14 +24,14 @@
   };
 
   programs.nixvim.keymaps = [
-    { mode = "n"; key = "<leader>gg"; action = "<cmd>lua Snacks.lazygit()<cr>"; options.desc = "Lazygit"; }
-    { mode = "n"; key = "<leader>gb"; action = "<cmd>lua Snacks.picker.git_branches()<cr>"; options.desc = "Git branches"; }
-    { mode = "n"; key = "<leader>gc"; action = "<cmd>lua Snacks.picker.git_log()<cr>"; options.desc = "Git log"; }
-    { mode = "n"; key = "<leader>gs"; action = "<cmd>lua Snacks.picker.git_status()<cr>"; options.desc = "Git status"; }
-    { mode = "n"; key = "<leader>ghb"; action = "<cmd>Gitsigns blame_line<cr>"; options.desc = "Blame line"; }
-    { mode = "n"; key = "<leader>ghR"; action = "<cmd>Gitsigns reset_buffer<cr>"; options.desc = "Reset buffer"; }
-    { mode = "n"; key = "<leader>ghr"; action = "<cmd>Gitsigns reset_hunk<cr>"; options.desc = "Reset hunk"; }
-    { mode = "n"; key = "<leader>ghs"; action = "<cmd>Gitsigns stage_hunk<cr>"; options.desc = "Stage hunk"; }
-    { mode = "n"; key = "<leader>ghu"; action = "<cmd>Gitsigns undo_stage_hunk<cr>"; options.desc = "Undo stage hunk"; }
+    { mode = "n"; key = "<leader>gg"; action = "<cmd>lua Snacks.lazygit()<cr>"; options.desc = "Lazygit (终端界面)"; }
+    { mode = "n"; key = "<leader>gb"; action = "<cmd>lua Snacks.picker.git_branches()<cr>"; options.desc = "查看分支"; }
+    { mode = "n"; key = "<leader>gc"; action = "<cmd>lua Snacks.picker.git_log()<cr>"; options.desc = "Git 日志"; }
+    { mode = "n"; key = "<leader>gs"; action = "<cmd>lua Snacks.picker.git_status()<cr>"; options.desc = "Git 状态"; }
+    { mode = "n"; key = "<leader>ghb"; action = "<cmd>Gitsigns blame_line<cr>"; options.desc = "行 Blame"; }
+    { mode = "n"; key = "<leader>ghR"; action = "<cmd>Gitsigns reset_buffer<cr>"; options.desc = "重置整个文件"; }
+    { mode = "n"; key = "<leader>ghr"; action = "<cmd>Gitsigns reset_hunk<cr>"; options.desc = "重置代码块"; }
+    { mode = "n"; key = "<leader>ghs"; action = "<cmd>Gitsigns stage_hunk<cr>"; options.desc = "暂存代码块"; }
+    { mode = "n"; key = "<leader>ghu"; action = "<cmd>Gitsigns undo_stage_hunk<cr>"; options.desc = "撤回暂存"; }
   ];
 }

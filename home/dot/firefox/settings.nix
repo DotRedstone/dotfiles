7e259a28e7e6920ee1b0577ecd9e269304a5a0bb
dotredstone <2724236945@qq.com>
2026-07-07 01:42:19 +0800

    feat(theme): 迁移中文字体至自用方正悠健黑体 (FZYJHK B)

--- home/dot/firefox/settings.nix
+++ home/dot/firefox/settings.nix
@@ -19,13 +19,13 @@
 
     # [Typography]
     # Matching Warden's system-wide fonts
-    "font.name.sans-serif.zh-CN" = "LXGW Neo XiHei";
-    "font.name.serif.zh-CN" = "LXGW WenKai";
+    "font.name.sans-serif.zh-CN" = "FZYJHK B";
+    "font.name.serif.zh-CN" = "FZYJHK B";
     "font.name.monospace.zh-CN" = "Maple Mono NF";
     "font.default.zh-CN" = "sans-serif";
 
     "font.name.sans-serif.x-western" = "Inter";
-    "font.name.serif.x-western" = "LXGW WenKai";
+    "font.name.serif.x-western" = "FZYJHK B";
     "font.name.monospace.x-western" = "Maple Mono NF";
     "font.default.x-western" = "sans-serif";
 

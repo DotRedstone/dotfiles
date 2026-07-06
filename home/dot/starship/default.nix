c7fa3ecddffaa3c74aab8c03e101c1118a04038b
dotredstone <2724236945@qq.com>
2026-07-07 02:17:48 +0800

    fix(starship): 修复 Noctalia v4 导致 starship.toml 重复键和冲突问题

--- home/dot/starship/default.nix
+++ home/dot/starship/default.nix
@@ -11,9 +11,4 @@
     enableFishIntegration = true;
   };
 
-  # [Dynamic Theme Link]
-  # Symlink to noctalia-generated cache for real-time color updates
-  xdg.configFile."starship.toml" = {
-    source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.cache/starship.toml");
-  };
 }

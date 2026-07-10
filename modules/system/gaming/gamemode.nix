1ea7e36dbe69d8602d48154dd74ee55353cb4dd0
dotredstone <2724236945@qq.com>
2026-07-10 19:03:02 +0800

    revert(gaming): 移除无法在 RedmiBook Pro 上生效的电源模式联动

--- modules/system/gaming/gamemode.nix
+++ modules/system/gaming/gamemode.nix
@@ -4,14 +4,8 @@
 # Scope: System
 # ---
 
-{ pkgs, ... }: {
+{ ... }: {
   programs.gamemode = {
     enable = true;
-    settings = {
-      custom = {
-        start = "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance";
-        end = "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced";
-      };
-    };
   };
 }

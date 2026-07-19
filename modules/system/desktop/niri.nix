af38f29a394582cafa3d9cf44a3750cc61e6314d
dotredstone <2724236945@qq.com>
2026-07-19 10:08:24 +0800

    fix(niri): 修复会议共享和全屏规则

--- modules/system/desktop/niri.nix
+++ modules/system/desktop/niri.nix
@@ -7,7 +7,17 @@
 { pkgs, inputs, ... }:
 
 let
-  niriPackage = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri;
+  niriPackage = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri.overrideAttrs (old: {
+    version = "${old.version or "unstable"}-shm-sharing";
+    __intentionallyOverridingVersion = true;
+    patches = (old.patches or [ ]) ++ [
+      (pkgs.fetchpatch {
+        name = "niri-shm-sharing-f3207c77095114f9f2202ca7db8333d4c1a958d1.patch";
+        url = "https://github.com/rucnyz/niri/commit/f3207c77095114f9f2202ca7db8333d4c1a958d1.patch";
+        hash = "sha256-u7OEbVtePF21Phr8aTV0LZPFdDHu1Ju9Jnv+Sd33xDI=";
+      })
+    ];
+  });
 in
 {
   programs.niri = {

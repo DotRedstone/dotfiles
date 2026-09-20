b0da9b15bc7be84f698f7909e5e621cdb3d3fd94
dotredstone <2724236945@qq.com>
2026-09-21 01:48:58 +0800

    fix(dev): 修正 virtme-ng 客体初始化解释器

--- home/dot/dev/kernel.nix
+++ home/dot/dev/kernel.nix
@@ -18,6 +18,13 @@ let
       hash = "sha256-/R+2ND/N+exF9eDSxAN8LR3cDuxBvpGSkiXcckyq8TY=";
     };
 
+    # The guest runs this script as PID 1 and NixOS has no /bin/bash.
+    # /nix/store remains visible in virtme-ng's copy-on-write host root.
+    postPatch = ''
+      substituteInPlace virtme/guest/virtme-init \
+        --replace-fail '#!/bin/bash' '#!${pkgs.bash}/bin/bash'
+    '';
+
     build-system = with pkgs.python3Packages; [
       argparse-manpage
       setuptools
